781583cccb99c4c8482a846f99cb29deabec8c9b
Ay-yoobD <dayoobwooday@gmail.com>
2025-06-24 23:29:47 +0200

    Collapsable Sidebar implemented

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,24 @@
 <template>
-  <div class="d-flex"> <!-- Bootstrap's flex container -->
+  <div class="d-flex">
     <NavBar />
-    <div class="content flex-grow-1 p-3">
+    <div class="content flex-grow-1 p-3 position-relative">
+      <button class="btn NavButton" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
+        ☰ 
+      </button>
       <router-view />
     </div>
   </div>
 </template>
 
 <script>
-  import NavBar from "./components/NavBar.vue";
+import NavBar from "./components/NavBar.vue";
 
-  export default {
-    components:{
-      NavBar
-    }
-    
+export default {
+  components: {
+    NavBar
   }
+
+}
 </script>
 
 <style>
@@ -26,6 +29,17 @@
   text-align: center;
   color: #2c3e50;
   min-height: 100vh;
+}
+
+.NavButton{
+  background-color:#0c2c47;
+  color: #97d3cd  ;
+  font-size: large;
+  position: absolute;
+  left: 10px;
+  top: 10px;
+
+
 }
 
 </style>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,53 +1,75 @@
 <template>
-    <div class="MainSideBar d-flex flex-column flex-shrink-0 p-3 text-white " style="width: 280px;">
-        <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-            <span class="fs-4">Sidebar</span>
-        </a>
-        <hr class="hrNav">
-        <ul class="InnerSideBar nav nav-pills flex-column mb-auto">
-            <li class="nav-item Links">
-                <router-link to="/" class="nav-link text-info"  aria-current="page">
-                    Home
-                </router-link>
-            </li>
-            <li class="Links">
-                <router-link to="/employees" class="nav-link text-white">
-                    Employee Dashboard
-                </router-link>
-            </li>
-            <li class="Links">
-                <router-link to="/payroll" class="nav-link text-white">
-                    Payroll Regulation
-                </router-link>
-            </li>
-            <li class="Links">
-                <router-link to="/timeoff" class="nav-link text-white">
-                   Timeoff Requests
-                </router-link>
-            </li>
-            <li class="Links">
-                <router-link to="/review" class="nav-link text-white">
-                    Performance Reviews
-                </router-link>
-            </li>
-        </ul>
-        <hr class="hrNav"   >
-        <div class="dropdown">
-            <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle"
-                id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-                <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2">
-                <strong>mdo</strong>
+
+
+
+
+
+    <div class="offcanvas offcanvas-start maincollapse" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel" style="width: 280px; min-height: 100vh;     background: linear-gradient(to bottom, #0c2c47 55%, #7ea1af 100%);">
+        <div class="offcanvas-header">
+            <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+                <span class="fs-4">Site Navigation</span>
             </a>
-            <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
-                <li><a class="dropdown-item" href="#">New project...</a></li>
-                <li><a class="dropdown-item" href="#">Settings</a></li>
-                <li><a class="dropdown-item" href="#">Profile</a></li>
-                <li>
-                    <hr class="dropdown-divider">
-                </li>
-                <li><a class="dropdown-item" href="#">Sign out</a></li>
-            </ul>
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
         </div>
+        <div class="offcanvas-body">
+            <div>
+                <div id="sidebar" class="MainSideBar d-flex flex-column flex-shrink-0 text-white collapse show">
+
+                    <ul class="InnerSideBar nav nav-pills flex-column mb-auto">
+                        <li class="nav-item Links" data-bs-dismiss="offcanvas">
+                            <router-link to="/" class="nav-link text-info" aria-current="page">
+                                Home
+                            </router-link>
+                        </li>
+                        <hr>
+                        <li class="Links" data-bs-dismiss="offcanvas">
+                            <router-link to="/employees" class="nav-link text-white">
+                                Employee Dashboard
+                            </router-link>
+                        </li>
+                        <hr>
+                        <li class="Links" data-bs-dismiss="offcanvas">
+                            <router-link to="/payroll" class="nav-link text-white">
+                                Payroll Regulation
+                            </router-link>
+                        </li>
+                        <hr>
+                        <li class="Links" data-bs-dismiss="offcanvas">
+                            <router-link to="/timeoff" class="nav-link text-white">
+                                Timeoff Requests
+                            </router-link>
+                        </li>
+                        <hr>
+                        <li class="Links" data-bs-dismiss="offcanvas">
+                            <router-link to="/review" class="nav-link text-white">
+                                Performance Reviews
+                            </router-link>
+                        </li>
+                    </ul>
+                    
+                    <!-- <hr class="hrNav"> -->
+
+                </div>
+            </div>
+
+        </div>
+                            <div class="dropdown offcanvas-footer p-3">
+                        <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle"
+                            id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+                            <img src="https://github.com/mdo.png" alt="" width="32" height="32"
+                                class="rounded-circle me-2">
+                            <strong>mdo</strong>
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
+                            <li><a class="dropdown-item" href="#">New project...</a></li>
+                            <li><a class="dropdown-item" href="#">Settings</a></li>
+                            <li><a class="dropdown-item" href="#">Profile</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">Sign out</a></li>
+                        </ul>
+                    </div>
     </div>
 
 </template>
@@ -59,20 +81,20 @@ export default {
 </script>
 
 <style>
-
-
- .MainSideBar{
+/* .MainSideBar {
     min-height: 100vh;
-    background: linear-gradient(to bottom,#0c2c47 55%,#7ea1af 100% );
+    background: linear-gradient(to bottom, #0c2c47 55%, #7ea1af 100%);
     border-right: #0b273e solid;
 
- }
- .hrNav{
-    color:#00070d;
- }
+} */
 
- .Links a{
-    margin: 20px;  
-    
- }
+.hrNav {
+    color: #00070d;
+}
+
+.Links a {
+  
+
+}
+
 </style>
